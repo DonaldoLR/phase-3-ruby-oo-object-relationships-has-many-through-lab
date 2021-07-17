312d9bdc51309545876825858f2108de876376f4
github username <github email address>
2021-07-17 15:06:29 -0400

    practice writing methods with has many through relationships in ruby

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,24 @@
+class Doctor
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
 
+  def self.all 
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def patients 
+    appointments.collect {|appointment| appointment.patient}
+  end
+end
